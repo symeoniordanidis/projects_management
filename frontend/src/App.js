9d306a47409c999e9fd5b2023b8e9c694b4f4c3e
syiordan <syiordan.dev@gmail.com>
2024-12-01 21:14:14 +0200

    added project cards in main page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import React,{useState,useEffect} from 'react';
+import axios from 'axios';
 import TeamForm from './components/TeamForm';
+import ProjectCard from './components/ProjectCard';
+
+
 
 function App() {
+  const API_URL = 'http://127.0.0.1:8000/api/projects/';
+  const [projects,setProjects] = useState([]);
+
+  useEffect(()=>{
+   axios.get(`${API_URL}`)
+   .then(response =>{
+     setProjects(response.data.results);
+   })
+   .catch(error=>{
+      console.error('There was an error while getting the projects!',error);
+   });
+  })
+  
+
   return (
     <div className="App">
       <TeamForm></TeamForm>
+      {projects.map((project) => (
+        <ProjectCard key={project.id} project={project}></ProjectCard>
+      ))}
+
     </div>
   );
 }
