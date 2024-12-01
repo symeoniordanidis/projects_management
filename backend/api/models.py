1d0c7ebae8894acbcdcba2f5896ea23b253ee31a
syiordan <syiordan.dev@gmail.com>
2024-12-01 16:24:32 +0200

    added models and serializers

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+STATUSES = (
+    ('new','New'),
+    ('in_progress','In Progress'),
+    ('completed','Completed'),
+    ('cancelled','Cancelled')
+)
+
+
+class Project(models.Model):
+    title = models.Charfield(max_length=100,blank=True)
+    description = models.TextField(blank=True)
+    start_date=models.DateField(blank=False)
+    last_updated_date = models.DateField(auto_now=True)
+    team = models.ForeignKey('Team',on_delete = models.SET_NULL,null=True,related_name='projects')
+    status = models.CharField(max_length=25,
+                              choices=STATUSES,
+                              default='new',
+                              blank=False)
+    def __str__(self):
+        return self.title
+
+
+class Team(models.Model):
+    name = models.CharField(max_length=100,blank=False)
+
+    def __str__(self):
+        return self.name
