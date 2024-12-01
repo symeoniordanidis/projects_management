134db579ade371f8ad35b546bfead0b01b2976c5
syiordan <syiordan.dev@gmail.com>
2024-12-01 16:28:49 +0200

    added viewsets in views.py

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from .models import Project,Team
+from rest_framework import viewsets
+from .serializers import TeamSerializer,ProjectSerializer
 
-# Create your views here.
+class ProjectViewSet(viewsets.ModelViewSet):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
+
+class TeamViewSet(viewsets.ModelViewSet):
+    queryset = Team.objects.all()
+    serializer_class = TeamSerializer
