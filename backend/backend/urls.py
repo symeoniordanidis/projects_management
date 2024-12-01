849824ff57afd06018a4d0d17b34b9d472b3a5c4
syiordan <syiordan.dev@gmail.com>
2024-12-01 16:35:14 +0200

    added routers

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -15,8 +15,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+from rest_framework import routers
+from api.views import ProjectViewSet, TeamViewSet
 
+
+api_router = routers.DefaultRouter()
+api_router.register('projects',ProjectViewSet)
+api_router.register('teams',TeamViewSet
+                    )
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/',include(api_router.urls))
 ]
